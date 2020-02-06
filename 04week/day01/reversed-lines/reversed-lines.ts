5b6bfc982d96c29d5370315ace6ddbebf64f9080
graflandras <graflandras@gmail.com>
2020-02-06 23:36:08 +0100

    reversed-lines/

---
+++ 04week/day01/reversed-lines/reversed-lines.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+// Create a method that decrypts reversed-lines.txt
+
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+    try {
+        return fs.readFileSync(fileName, 'utf-8');
+    } catch (e) {
+        console.log(e.message);
+        return null;
+    }
+}
+function reversedLines(fileName: string): string {
+
+    let fileContent: string[] = readFromFile(fileName).split('\r\n');
+    let reverse: string[] = [];
+
+    fileContent.forEach(function (element) {
+        reverse.push(element.split('').reverse().join(''));
+    });
+    return reverse.join('\r\n');
+}
+
+console.log(reversedLines("reversed-lines.txt"));
